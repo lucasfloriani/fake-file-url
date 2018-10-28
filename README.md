229bb572daa4cc848b4bcdd36177fd15cc95c4ea
lucasfloriani <lucas.floriane@gmail.com>
2018-10-27 21:35:22 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fake File URL
+
+This package transforms your base64 content file in a fake url to use in frontend url tags.
---
+++ index.js
@@ -0,0 +1,10 @@
+export const createBlob = (fileContent, fileType) => {
+  const byteCharacters = atob(fileContent);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i += 1) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  const blob = new Blob([byteArray], { type: fileType });
+  return URL.createObjectURL;
+};
